e31919db6df20cf6bfa703b9a3830f5b6a78a096
224259 Biechoński Łukasz <224259@edu.p.lodz.pl>
2021-05-18 00:16:53 +0200

    getPossibleMoves() in King, Queen, Bishop

--- LogicStructure/src/main/java/King.java
+++ LogicStructure/src/main/java/King.java
@@ -1,2 +1,76 @@
-public class King extends Piece{
+public class King extends Piece {
+
+    boolean wasMoved = false;
+
+    public King(int x, int y, PieceColor color, int value) {
+        super(x, y, color, value);
+    }
+
+
+    @Override
+    public Move[] getPossibleMoves() {
+        Move[] possibleMoves = new Move[8];
+        int tempX = 0;
+        int tempY = 0;
+        int counter = 0;
+
+        for (int direction = 0; direction < 8; direction++) {
+            tempX = currentX;
+            tempY = currentY;
+
+            if (direction == 0) {
+                if (tempX > 0 && tempY > 0) {
+                    tempX--;
+                    tempY--;
+                    possibleMoves[counter] = new Move(currentX, currentY, tempX, tempY);
+                }
+            } else if (direction == 1) {
+                if ( tempY > 0) {
+                    tempY--;
+                    possibleMoves[counter] = new Move(currentX, currentY, tempX, tempY);
+                    counter++;
+                }
+            } else if (direction == 2) {
+                if (tempX < 7 && tempY > 0) {
+                    tempX++;
+                    tempY--;
+                    possibleMoves[counter] = new Move(currentX, currentY, tempX, tempY);
+                    counter++;
+                }
+            } else if (direction == 3) {
+                if (tempX < 7 ) {
+                    tempX++;
+                    possibleMoves[counter] = new Move(currentX, currentY, tempX, tempY);
+                    counter++;
+                }
+            } else if (direction == 4) {
+                if (tempX < 7 && tempY < 7) {
+                    tempX++;
+                    tempY++;
+                    possibleMoves[counter] = new Move(currentX, currentY, tempX, tempY);
+                    counter++;
+                }
+            } else if (direction == 5) {
+                if (tempY < 7) {
+                    tempY++;
+                    possibleMoves[counter] = new Move(currentX, currentY, tempX, tempY);
+                    counter++;
+                }
+            } else if (direction == 6) {
+                if (tempX > 0 && tempY < 7) {
+                    tempX--;
+                    tempY++;
+                    possibleMoves[counter] = new Move(currentX, currentY, tempX, tempY);
+                    counter++;
+                }
+            } else if (direction == 7) {
+                if (tempX > 0 ) {
+                    tempX--;
+                    possibleMoves[counter] = new Move(currentX, currentY, tempX, tempY);
+                    counter++;
+                }
+            }
+        }
+        return possibleMoves;
+    }
 }
