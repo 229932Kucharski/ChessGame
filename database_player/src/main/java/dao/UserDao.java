a77814da71f6db439fcb93d4b57d8250d6b80c20
229932 Grzegorz Kucharski <229932@p.lodz.pl>
2021-05-18 11:45:17 +0200

    classes UserDao, BoardDao. Added .iml to gitignore

---
+++ database_player/src/main/java/dao/UserDao.java
@@ -0,0 +1,40 @@
+package dao;
+
+import player.User;
+
+import java.util.List;
+
+public class UserDao implements Dao<User>{
+
+
+
+    @Override
+    public void add(User obj) {
+
+    }
+
+    @Override
+    public User findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public void delete(User obj) {
+
+    }
+
+    @Override
+    public void update(User obj) {
+
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+}
