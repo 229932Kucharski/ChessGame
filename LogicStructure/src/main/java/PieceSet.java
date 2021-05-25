52c8ad504682146c884a791f3b9dbf0c6aa5f061
Wojciech Baljon <229838@edu.p.lodz.pl>
2021-05-25 21:11:01 +0200

    PieceSet constructor finished

--- LogicStructure/src/main/java/PieceSet.java
+++ LogicStructure/src/main/java/PieceSet.java
@@ -10,24 +10,35 @@ public class PieceSet {
     public PieceSet(PieceColor pieceColor) {
         this.pieceColor = pieceColor;
         int id = 0;
+        int pawnY = 0;
+        int kingY = 0;
         if(pieceColor == PieceColor.WHITE) {
-            for(int x = 0; x < 8; x ++) {
-                pieces.add(new Pawn(x, 1, pieceColor, id));
-                id++;
-            }
-            pieces.add(new Rook(0, 0, pieceColor, id));
-            id++;
-            pieces.add(new Knight(1, 0, pieceColor, id));
-            id++;
-//            pieces.add(new Bishop(2, 0, pieceColor, id));
-//            id++;
-//            pieces.add(new Bishop(5, 0, pieceColor, id));
-//            id++;
-            pieces.add(new Knight(6, 0, pieceColor, id));
-            id++;
-            pieces.add(new Rook(7, 0, pieceColor, id));
-
+            pawnY = 1;
+            kingY = 0;
+        } else {
+            kingY = 7;
+            pawnY = 6;
         }
+
+        for(int x = 0; x < 8; x ++) {
+            pieces.add(new Pawn(x, pawnY, pieceColor, id));
+            id++;
+        }
+        pieces.add(new Rook(0, kingY, pieceColor, id));
+        id++;
+        pieces.add(new Knight(1, kingY, pieceColor, id));
+        id++;
+        pieces.add(new Bishop(2, kingY, pieceColor, id));
+        id++;
+        pieces.add(new Queen(3, kingY, pieceColor, id));
+        id++;
+        pieces.add(new King(4, kingY, pieceColor, id));
+        id++;
+        pieces.add(new Bishop(5, kingY, pieceColor, id));
+        id++;
+        pieces.add(new Knight(6, 7, pieceColor, id));
+        id++;
+        pieces.add(new Rook(7, 7, pieceColor, id));
     }
 
     public boolean move(Move move) {
