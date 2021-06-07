57f2f394b11921c650b62d3e976531d384652dc2
224259 Biechoński Łukasz <224259@edu.p.lodz.pl>
2021-06-07 20:36:53 +0200

    fixed conflict with merge

--- LogicStructure/src/main/java/Algorithm/Easy.java
+++ LogicStructure/src/main/java/Algorithm/Easy.java
@@ -14,14 +14,11 @@ public class Easy extends Algorithm{
     @Override
     public void makeMove() {
 
-//<<<<<<< HEAD
-//        List<Move> moves = allySet.getPossibleMoves();
-//        Move nextMove =  getRandomMove(moves);
-//        //gameSet.move(nextMove);
-//=======
-//      // gameSet.move(nextMove);
-//>>>>>>> 25a33cf452e00514943d7c7ed2c79737e12d3693
-//
+        List<Move> moves = allySet.getPossibleMoves();
+        Move nextMove =  getRandomMove(moves);
+        allySet.move(nextMove, enemySet);
+
+
 
     }
 }
